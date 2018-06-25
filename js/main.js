a573d236b4d0ace52242ec421d46bc37f82721dd
Tim Caldwell <tim.caldwell@einti.com>
2018-06-25 17:22:23 +1000

    add rudimentary unbeatable ai + turn bug

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ let player = "naught";
 let naughtIco = '<i class="far fa-circle"></i>'
 let crossIco = '<i class="fas fa-times"></i>'
 
+let aiEnabled = true;
+let aiTurn = false;
+
 const makeGameBoard = function () { // populate array of slots in gameData and create DOM.
   for (let x = 0; x < 3; x++ ) {
     gameData.push([]);
@@ -17,10 +20,10 @@ const makeGameBoard = function () { // populate array of slots in gameData and c
 
 const winDisplay = function ( winInfo ) {
   const slots = winInfo.slots;
-  console.log(slots);
+  //console.log(slots);
   for ( let slot of slots ) {
     const slotID = slot.x + "," + slot.y
-    console.log(slotID);
+    //console.log(slotID);
     $( `div[data-id="${ slotID }"]` ).addClass('win');
   }
 
@@ -30,6 +33,7 @@ const winDisplay = function ( winInfo ) {
 const takeSlot = function () {
   let id = $( this ).attr('data-id').split(',');
   gameData[+id[0]][+id[1]] = player;
+
   switch (player) {
     case 'naught':
         $( this ).append( $( naughtIco ) )
@@ -41,6 +45,7 @@ const takeSlot = function () {
 
   $( this ).off( 'click', takeSlot );
 
+  // debugger;
   let winInfo = checkForWin()
   if ( winInfo.win === true ) {
     winDisplay( winInfo );
@@ -52,115 +57,205 @@ const takeSlot = function () {
   } else {
     player = 'naught';
   }
+
+  if ( aiEnabled === true && aiTurn === true ) {
+    aiTurn = false;
+  } else if ( aiEnabled === true && aiTurn === false ) {
+    aiTurn = true;
+    setTimeout ( function () {
+      unbeatableAI();
+
+      player = 'naught'
+    }, 1000 );
+  }
 }
 
-const checkYforWin = function () {
+const checkYforWin = function ( player ) {
 // check y straights for win
   let win = false;
   const winSlots = [];
+  const aiSlots = [];
+  const emptySlots = [];
+
   for ( let y = 0; y < 3; y++ ) {
     for ( let x = 0; x < 3; x++) {
       if ( gameData[x][y] === player ) {
-        win = true;
-        winSlots.push({x:x,y:y})
-      } else {
-        win = false;
-        winSlots.length = 0;
-        break;
+        winSlots.push({ x:x, y:y })
+      } else if ( gameData[x][y] === null ) {
+        emptySlots.push({ x:x, y:y })
       }
     }
 
-    if(win) {
+    if (aiEnabled === true && winSlots.length === 2) {
+      Array.prototype.push.apply(aiSlots, emptySlots);
+    }
+
+    if(winSlots.length === 3) {
+      win = true;
       break;
+    } else {
+      winSlots.length = 0;
     }
   }
 
-  return {win: win, slots: winSlots};
+  return {win: win, slots: winSlots, aiSlots: aiSlots };
 }
 
-const checkXforWin = function () {
+const checkXforWin = function ( player ) {
   // check x straights for win
   let win = false;
   const winSlots = [];
+  const aiSlots = [];
+  const emptySlots = [];
 
   for ( let x = 0; x < 3; x++) {
     for ( let y = 0; y < 3; y++ ) {
       if (gameData[x][y] === player) {
-        win = true;
-        winSlots.push({x:x,y:y})
-      } else {
-        win = false;
-        winSlots.length = 0;
-        break;
+        winSlots.push({ x:x, y:y });
+      } else if ( gameData[x][y] === null ) {
+        emptySlots.push({ x:x ,y:y })
       }
     }
 
-    if (win) {
+    if (aiEnabled === true && winSlots.length === 2) {
+      Array.prototype.push.apply(aiSlots, emptySlots);
+    }
+
+    if(winSlots.length === 3) {
+      win = true;
       break;
+    } else {
+      winSlots.length = 0;
     }
   }
 
-  return {win: win, slots: winSlots};
+  return {win: win, slots: winSlots, aiSlots: aiSlots};
 }
 
-const diag1ForWin = function () {
+const diag1ForWin = function ( player ) {
   // check corner x=0,y=0 > x=2, y=2 for win
   let win = false;
   const winSlots = [];
+  const aiSlots = [];
+  const emptySlots = [];
 
   for ( let xy = 0; xy < 3; xy++ ) {
     if (gameData[xy][xy] === player) {
-      win = true;
       winSlots.push({x:xy,y:xy})
-    } else {
-      win = false;
-      winSlots.length = 0;
-      break;
+    } else if ( gameData[xy][xy] === null ) {
+      emptySlots.push({x:xy,y:xy})
     }
   }
 
-  return {win: win, slots: winSlots};
+  if (aiEnabled === true && winSlots.length === 2) {
+    Array.prototype.push.apply(aiSlots, emptySlots);
+  }
+
+  if(winSlots.length === 3) {
+    win = true;
+  }
+
+  return {win: win, slots: winSlots, aiSlots: aiSlots };
 }
 
-const diag2ForWin = function () {
+const diag2ForWin = function ( player ) {
   // check corner x=2, y=0 > x=0, y=2, for win
   let win = false;
   const winSlots = [];
-
+  const aiSlots = [];
+  const emptySlots = [];
   for (let x = 0; x < 3; x++ ) {
     let y = 2 - x;
     if (gameData[x][y] === player) {
-      win = true;
       winSlots.push({x:x,y:y})
-    } else {
-      win = false;
-      winSlots.length = 0;
-      break;
+    } else if ( gameData[x][y] === null ) {
+      emptySlots.push({x:x,y:y})
     }
   }
+  if (aiEnabled === true && winSlots.length === 2 && gameData[x][y] === null) {
+      Array.prototype.push.apply(aiSlots, emptySlots);
+  }
+  if(winSlots.length === 3) {
+    win = true;
+  }
 
-  return {win: win, slots: winSlots};
+  return {win: win, slots: winSlots, aiSlots: aiSlots};
+};
+
+const aiClickTrigger = function ( win ) {
+  slotID = win.aiSlots[0].x + ',' + win.aiSlots[0].y;
+  console.log('triggering AI on slot:', slotID);
+  $( `div[data-id="${ slotID }"]` ).trigger( 'click' )
+};
+
+const getRandomInt = function(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+const unbeatableAI = function () {
+  debugger;
+
+  let win = checkYforWin('naught');
+  if (win.aiSlots.length > 0) {
+    aiClickTrigger( win );
+    return;
+  }
+
+  win = checkXforWin('naught');
+  if (win.aiSlots.length > 0) {
+    aiClickTrigger( win );
+    return;
+  }
+
+  win = diag1ForWin('naught');
+  if (win.aiSlots.length > 0) {
+    aiClickTrigger( win );
+    return;
+  }
+
+  win = diag2ForWin('naught');
+  if (win.aiSlots.length > 0) {
+    aiClickTrigger( win );
+    return;
+  }
+
+  //no danger of losing found above.
+  let freeSlot = false;
+  do {
+    win.aiSlots = [{
+      x: getRandomInt(3),
+      y: getRandomInt(3)
+    }];
+
+    if ( gameData[win.aiSlots[0].x][win.aiSlots[0].y] === null ) {
+      aiClickTrigger( win );
+      freeSlot = true;
+      break;
+    }
+  } while (!freeSlot)
 }
+
+
 
 const checkForWin = function () {
   // debugger;
 
-  let win = checkYforWin();
+  let win = checkYforWin( player );
   if (win.win === true) {
     return win;
   }
 
-  win = checkXforWin();
+  win = checkXforWin( player );
   if (win.win === true) {
     return win;
   }
 
-  win = diag1ForWin();
+  win = diag1ForWin( player );
   if (win.win === true ) {
     return win;
   }
 
-  win = diag2ForWin();
+  win = diag2ForWin( player );
   if (win.win === true ) {
     return win;
   }
