4cedb0fb262424613d873fd4ff399444aea88d82
Tim Caldwell <tim.caldwell@einti.com>
2018-06-26 10:29:32 +1000

    AI will try to prevent you from winning

--- js/main.js
+++ js/main.js
@@ -19,15 +19,18 @@ const makeGameBoard = function () { // populate array of slots in gameData and c
 }
 
 const winDisplay = function ( winInfo ) {
-  const slots = winInfo.slots;
-  //console.log(slots);
-  for ( let slot of slots ) {
-    const slotID = slot.x + "," + slot.y
-    //console.log(slotID);
-    $( `div[data-id="${ slotID }"]` ).addClass('win');
-  }
+  if ( winInfo.tie !== undefined ) {
+    // it's a draw!
+    $( '#gameboard' ).append( $( '<div>').addClass('winmsg').html(`Evenly matched! It's a draw!`) );
+  } else {
+    const slots = winInfo.slots;
+    for ( let slot of slots ) {
+      const slotID = slot.x + "," + slot.y;
+      $( `div[data-id="${ slotID }"]` ).addClass('win');
+    }
 
-  $( '#gameboard' ).append( $( '<div>').addClass('winmsg').html(`Congratulations! ${ player } wins!`) )
+    $( '#gameboard' ).append( $( '<div>').addClass('winmsg').html(`Congratulations! ${ player } wins!`) );
+  }
 }
 
 const takeSlot = function () {
@@ -41,11 +44,12 @@ const takeSlot = function () {
     default:
       $( this ).append( $( crossIco ) )
   }
+  console.log(player);
 
 
   $( this ).off( 'click', takeSlot );
 
-  // debugger;
+  debugger;
   let winInfo = checkForWin()
   if ( winInfo.win === true ) {
     winDisplay( winInfo );
@@ -57,21 +61,43 @@ const takeSlot = function () {
   } else {
     player = 'naught';
   }
+  console.log(player);
+
 
   if ( aiEnabled === true && aiTurn === true ) {
     aiTurn = false;
   } else if ( aiEnabled === true && aiTurn === false ) {
     aiTurn = true;
     setTimeout ( function () {
-      unbeatableAI();
-
-      player = 'naught'
+        // check to see if AI can win with this move
+        // check to see if player will win next move
+        if ( !unbeatableAI( 'naught' ) ) {
+          if ( !unbeatableAI( 'cross' ) ) {
+            pickRandomSlot();
+        }
+      }
     }, 1000 );
   }
 }
+const pickRandomSlot = function () {
+  let freeSlot = false;
+  const win = {};
+  do {
+    win.aiSlots = [{
+      x: getRandomInt(3),
+      y: getRandomInt(3)
+    }];
+
+    if ( gameData[win.aiSlots[0].x][win.aiSlots[0].y] === null ) {
+      aiClickTrigger( win );
+      freeSlot = true;
+      break;
+    }
+  } while (!freeSlot)
+}
 
 const checkYforWin = function ( player ) {
-// check y straights for win
+ // check y straights for win
   let win = false;
   const winSlots = [];
   const aiSlots = [];
@@ -172,7 +198,7 @@ const diag2ForWin = function ( player ) {
       emptySlots.push({x:x,y:y})
     }
   }
-  if (aiEnabled === true && winSlots.length === 2 && gameData[x][y] === null) {
+  if (aiEnabled === true && winSlots.length === 2) {
       Array.prototype.push.apply(aiSlots, emptySlots);
   }
   if(winSlots.length === 3) {
@@ -188,57 +214,56 @@ const aiClickTrigger = function ( win ) {
   $( `div[data-id="${ slotID }"]` ).trigger( 'click' )
 };
 
-const getRandomInt = function(max) {
+const getRandomInt = function( max ) {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-const unbeatableAI = function () {
-  debugger;
+const unbeatableAI = function ( player ) {
 
-  let win = checkYforWin('naught');
+  let win = checkYforWin( player );
   if (win.aiSlots.length > 0) {
     aiClickTrigger( win );
-    return;
+    return true;
   }
 
-  win = checkXforWin('naught');
+  win = checkXforWin( player );
   if (win.aiSlots.length > 0) {
     aiClickTrigger( win );
-    return;
+    return true;
   }
 
-  win = diag1ForWin('naught');
+  win = diag1ForWin( player );
   if (win.aiSlots.length > 0) {
     aiClickTrigger( win );
-    return;
+    return true;
   }
 
-  win = diag2ForWin('naught');
+  win = diag2ForWin( player );
   if (win.aiSlots.length > 0) {
     aiClickTrigger( win );
-    return;
+    return true;
   }
 
-  //no danger of losing found above.
-  let freeSlot = false;
-  do {
-    win.aiSlots = [{
-      x: getRandomInt(3),
-      y: getRandomInt(3)
-    }];
-
-    if ( gameData[win.aiSlots[0].x][win.aiSlots[0].y] === null ) {
-      aiClickTrigger( win );
-      freeSlot = true;
-      break;
-    }
-  } while (!freeSlot)
+  return false;
 }
-
-
 
 const checkForWin = function () {
   // debugger;
+
+  let anyFreeSlots = false;
+  // check if  all slots taken
+  for (let x = 0; x < 3; x++) {
+    for ( let y = 0; y < 3; y++ ) {
+      if ( gameData[x][y] === null ) {
+        anyFreeSlots = true;
+        break; // stop loop if there's at least one empty slot
+      }
+    }
+  }
+
+  if ( anyFreeSlots === false ) {
+    return { win: true, tie: true, slots: [], aiSlots: [] };
+  }
 
   let win = checkYforWin( player );
   if (win.win === true) {
